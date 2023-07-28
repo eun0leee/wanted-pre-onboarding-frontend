8a0796ee00d1f7f85fe9819d5b66419a5060d617
eun0leee <eun0leee@gmail.com>
2023-07-28 13:50:22 +0900

    feat : 로그인, 회원가입 링크버튼 생성

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,4 +1,23 @@
+import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
+
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <Container>
+      <Button to={'/signin'}>로그인</Button>
+      <Button to={'/signup'}>회원가입</Button>
+    </Container>
+  );
 };
 export default Main;
+
+const Container = styled.main``;
+
+const Button = styled(Link)`
+  padding: 10px 30px;
+  border-radius: 10px;
+  font-size: 14px;
+  color: ${({ theme }) => theme.colors.white};
+  font-weight: 700;
+  background-color: ${({ theme }) => theme.colors.green_main};
+`;
