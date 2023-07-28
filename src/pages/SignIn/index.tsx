c9273401c548cc9a5ee578614c65bdf57cc6b57c
eun0leee <eun0leee@gmail.com>
2023-07-28 15:34:24 +0900

    feat : 로그인

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,4 +1,13 @@
+import styled from '@emotion/styled';
+import Auth from 'src/components/Auth';
+
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <Container>
+      <Auth moveto="/todo" btnName="로그인" />
+    </Container>
+  );
 };
 export default SignIn;
+
+const Container = styled.main``;
