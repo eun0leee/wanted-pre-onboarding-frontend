f22ea9fce43e4fb33ab3c3bc93784594c91875af
eun0leee <eun0leee@gmail.com>
2023-07-28 15:33:23 +0900

    feat : 회원가입

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,4 +1,13 @@
+import styled from '@emotion/styled';
+import Auth from 'src/components/Auth';
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <Container>
+      <Auth moveto="/signin" btnName="회원가입" />
+    </Container>
+  );
 };
 export default SignUp;
+
+const Container = styled.main``;
