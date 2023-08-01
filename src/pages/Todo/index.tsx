a6e86fa4fbb25d8ee539fb556455a3603da4e775
eun0leee <eun0leee@gmail.com>
2023-08-01 22:10:43 +0900

    feat : todo 추가

--- src/pages/Todo/index.tsx
+++ src/pages/Todo/index.tsx
@@ -1,4 +1,59 @@
+import { useState } from 'react';
+
+interface ITodo {
+  id: number;
+  text: string;
+  check: boolean;
+}
+
 const Todo = () => {
-  return <div>Todo</div>;
+  const [text, setText] = useState('');
+  const [count, setCount] = useState(1);
+  const [todoList, setTodoList] = useState<ITodo[]>([]);
+
+  console.log(todoList);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const todo: ITodo = {
+      id: count,
+      text,
+      check: false,
+    };
+
+    setTodoList(todoList.concat(todo));
+    setCount(count + 1);
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          data-testid="new-todo-input"
+          type="text"
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button data-testid="new-todo-add-button" type="submit">
+          추가
+        </button>
+      </form>
+      <ul>
+        {todoList.map((todo) => (
+          <li key={todo.id}>
+            <label>
+              <input type="checkbox" />
+              <span>{todo.text}</span>
+              <button data-testid="modify-button" type="button">
+                수정
+              </button>
+              <button data-testid="delete-button" type="button">
+                삭제
+              </button>
+            </label>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 export default Todo;
